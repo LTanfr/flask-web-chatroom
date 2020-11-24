43d21174ef3d96545dfe36439950d13d6b994f51
LTanfr <576480782@qq.com>
2020-11-24 17:32:29 +0800

    chat-with-list init

--- chat_demo/app/routes.py
+++ chat_demo/app/routes.py
@@ -3,41 +3,33 @@ from flask import render_template, flash, redirect, url_for, request, current_ap
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Message
-from werkzeug.urls import url_parse
 
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('chat'))
+        return redirect(url_for('chat_with_list'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password.')
+            flash('用户名或密码错误。')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('chat'))
+        return redirect(url_for('chat_with_list'))
     return render_template('login.html', title='登 录', form=form)
-    # form = LoginForm()
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(username=form.username.data).first()
-    #     if user is None or not user.check_password(form.password.data):
-    #         flash('Invalid username or password')
-    #         return redirect(url_for('login'))
-    #     login_user(user, remember=form.remember_me.data)
-    #     next_page = request.args.get('next')
-    #     if not next_page or url_parse(next_page).netloc != '':
-    #         next_page = url_for('index')
-    #     return redirect(next_page)
-    # return render_template('login.html', title='登 录', form=form)
 
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+
+@app.route('/chat_with_list', methods=['POST', 'GET'])
+def chat_with_list():
+    return render_template('chat_with_list.html')
 
 
 @app.route('/chat', methods=['POST', 'GET'])
